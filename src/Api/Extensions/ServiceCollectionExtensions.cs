a26cd062cc193525756bb5bf3ae583562fb7623b
Marty Mathis <Marty.Mathis@assurant.com>
2020-12-21 21:51:58 -0500

    Updates to extensions with unit tests

--- src/Api/Extensions/ServiceCollectionExtensions.cs
+++ src/Api/Extensions/ServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.Azure.Cosmos.Fluent;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace AuthorizationManagement.Api.Extensions
 {
@@ -9,16 +10,44 @@ namespace AuthorizationManagement.Api.Extensions
     {
         public static IServiceCollection AddCosmosDb(this IServiceCollection services, IConfiguration config)
         {
-            services.AddSingleton(
-                new CosmosClientBuilder(config[config["CosmosDb:ConnectionStringKey"]])
-                    .WithSerializerOptions(new CosmosSerializationOptions { PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase })
-                    .WithApplicationRegion(Regions.NorthCentralUS)
-                    .WithConnectionMode(config["CosmosDb:ConnectionMode"])
-                    .Build());
+            if (!TryGetConnectionString(config, out var connectionString))
+                throw new InvalidOperationException("Could not obtain a connection string from configuration.");
 
+            var builder = new CosmosClientBuilder(connectionString)
+                    .WithSerializerOptions(new CosmosSerializationOptions { PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase })
+                    .WithConnectionMode(config["CosmosDb:ConnectionMode"]);
+
+            var regions = config.GetSection("CosmosDb:PreferredRegions").Get<string[]>();
+            if (regions?.Length > 0)
+                builder = builder.WithApplicationPreferredRegions(regions);
+
+            services.AddSingleton(builder.Build());
+            
             return services.AddSingleton(provider =>
                 provider.GetRequiredService<CosmosClient>()
                     .GetContainer(config["CosmosDb:DatabaseId"], config["CosmosDb:ContainerId"]));
+        }
+
+        private static bool TryGetConnectionString(IConfiguration config, out string connectionString)
+        {
+            connectionString = config["CosmosDb:ConnectionString"];
+            if (!string.IsNullOrWhiteSpace(connectionString)) return true;
+            
+            var connectionStringPath = config["CosmosDb:ConnectionStringConfigKeyPath"];
+            if (string.IsNullOrWhiteSpace(connectionStringPath)) return false;
+
+            connectionString = config[connectionStringPath];
+            return !string.IsNullOrWhiteSpace(connectionString);
+        }
+
+        private static CosmosClientBuilder WithConnectionMode(this CosmosClientBuilder builder, string connectionMode)
+        {
+            if (!Enum.TryParse(connectionMode, true, out ConnectionMode mode))
+                mode = ConnectionMode.Direct;
+
+            return mode == ConnectionMode.Direct
+                ? builder.WithConnectionModeDirect()
+                : builder.WithConnectionModeGateway();
         }
     }
 }
---
+++ tests/Api.Tests/Extensions/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,118 @@
+﻿using AuthorizationManagement.Api.Extensions;
+using FluentAssertions;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+namespace AuthorizationManagement.Api.Tests.Extensions
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class ServiceCollectionExtensionsTests
+    {
+        private IServiceCollection _services;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _services = new ServiceCollection();
+        }
+
+        [TestMethod]
+        public void AddCosmosDb_Test()
+        {
+            //Arrange
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(
+                    new Dictionary<string, string>
+                    {
+                        {"CosmosDb:ConnectionString", "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="},
+                        {"CosmosDb:DatabaseId", "db"},
+                        {"CosmosDb:ContainerId", "col"}
+                    })
+                .Build();
+
+            _services.AddCosmosDb(config);
+            var provider = _services.BuildServiceProvider(new ServiceProviderOptions { ValidateOnBuild = true });
+            
+            //Act
+            var result = provider.GetService<Container>();
+            
+            //Assert
+            result.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void AddCosmosDb_AlternateConnectionStringConfig_Test()
+        {
+            //Arrange
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(
+                    new Dictionary<string, string>
+                    {
+                        {"CosmosDb:ConnectionStringConfigKeyPath", "Other:CosmosB:Config:Connection" },
+                        {"Other:CosmosB:Config:Connection", "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="},
+                        {"CosmosDb:DatabaseId", "db"},
+                        {"CosmosDb:ContainerId", "col"}
+                    })
+                .Build();
+
+            _services.AddCosmosDb(config);
+            var provider = _services.BuildServiceProvider(new ServiceProviderOptions { ValidateOnBuild = true });
+
+            //Act
+            var result = provider.GetService<Container>();
+
+            //Assert
+            result.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void AddCosmosDb_MissingConnectionString_Test()
+        {
+            //Arrange
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>())
+                .Build();
+
+            //Act
+            Action act = () => _services.AddCosmosDb(config);
+
+            //Assert
+            act.Should()
+                .ThrowExactly<InvalidOperationException>()
+                .WithMessage("Could not obtain a connection string from configuration.");
+        }
+
+        [TestMethod]
+        public void AddCosmosDb_WithPreferredRegions_Test()
+        {
+            //Arrange
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(
+                    new Dictionary<string, string>
+                    {
+                        {"CosmosDb:ConnectionString", "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="},
+                        {"CosmosDb:DatabaseId", "db"},
+                        {"CosmosDb:ContainerId", "col"}
+                    })
+                .SetBasePath(Path.Combine(AppContext.BaseDirectory, "Extensions"))
+                .AddJsonFile("testsettings.json", false, false)
+                .Build();
+
+            _services.AddCosmosDb(config);
+            var provider = _services.BuildServiceProvider(new ServiceProviderOptions { ValidateOnBuild = true });
+
+            //Act
+            var result = provider.GetService<Container>();
+
+            //Assert
+            result.Should().NotBeNull();
+        }
+    }
+}
